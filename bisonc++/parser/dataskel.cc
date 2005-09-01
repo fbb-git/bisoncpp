21bb6ca3cb0803d83f5edc1ec68be0d7a1533837
Frank B. Brokken <f.b.brokken@rug.nl>
2005-09-01 20:59:50 +0000

    *** empty log message ***
    git-svn-id: https://bisoncpp.svn.sourceforge.net/svnroot/bisoncpp/trunk@25 264705d0-3b1c-0410-8509-e6d6d96411b3

--- bisonc++/parser/dataskel.cc
+++ bisonc++/parser/dataskel.cc
@@ -1,10 +1,7 @@
 #include "parser.ih"
 #include "SKEL"
 
-char Parser::s_defaultBaseclassSkeleton[] = _Skel_ "/bisonc++base.h";
-char Parser::s_defaultClassSkeleton[]    = 
-            "/usr/share/bisonc++/bisonc++.h";
-char Parser::s_defaultImplementationSkeleton[]    = 
-            "/usr/share/bisonc++/bisonc++.ih";
-char Parser::s_defaultParsefunSkeleton[]    = 
-            "/usr/share/bisonc++/bisonc++.cc";
+char Parser::s_defaultBaseclassSkeleton[]       = _Skel_ "/bisonc++base.h";
+char Parser::s_defaultClassSkeleton[]           = _Skel_ "/bisonc++.h";
+char Parser::s_defaultImplementationSkeleton[]  = _Skel_ "/bisonc++.ih";
+char Parser::s_defaultParsefunSkeleton[]        = _Skel_ "/bisonc++.cc";
