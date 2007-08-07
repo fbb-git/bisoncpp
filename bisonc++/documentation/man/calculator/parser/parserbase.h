7553094428a852b493dd377fabaa6d797f237fc8
Frank B. Brokken <f.b.brokken@rug.nl>
2007-08-07 10:33:54 +0000

    Manual page used 1.6.1 parserbase.h file: recreated that file
    git-svn-id: https://bisoncpp.svn.sourceforge.net/svnroot/bisoncpp/trunk@296 264705d0-3b1c-0410-8509-e6d6d96411b3

--- bisonc++/documentation/man/calculator/parser/parserbase.h
+++ bisonc++/documentation/man/calculator/parser/parserbase.h
@@ -7,7 +7,7 @@
 
 namespace // anonymous
 {
-    struct PI;
+    struct PI__;
 }
 
 
@@ -17,7 +17,7 @@ class ParserBase
 // $insert tokens
 
     // Symbolic tokens:
-    enum Tokens
+    enum Tokens__
     {
         NUMBER = 257,
         EOLN,
@@ -25,43 +25,49 @@ class ParserBase
     };
 
 // $insert STYPE
-    typedef int STYPE;
+typedef int STYPE__;
 
     private:
-        int d_stackIdx;
-        std::vector<size_t>   d_stateStack;
-        std::vector<STYPE>    d_valueStack;
+        int d_stackIdx__;
+        std::vector<size_t>   d_stateStack__;
+        std::vector<STYPE__>  d_valueStack__;
 
     protected:
-        enum Return
+        enum Return__
         {
-            PARSE_ACCEPT = 0,   // values used as parse()'s return values
-            PARSE_ABORT  = 1
+            PARSE_ACCEPT__ = 0,   // values used as parse()'s return values
+            PARSE_ABORT__  = 1
         };
-        enum ErrorRecovery
+        enum ErrorRecovery__
         {
-            DEFAULT_RECOVERY_MODE,
-            UNEXPECTED_TOKEN,
+            DEFAULT_RECOVERY_MODE__,
+            UNEXPECTED_TOKEN__,
         };
-        bool        d_debug;
-        size_t    d_nErrors;
-        int         d_token;
-        int         d_nextToken;
-        size_t    d_state;
-        STYPE      *d_vsp;
-        STYPE       d_val;
+        bool        d_debug__;
+        size_t      d_nErrors__;
+        size_t      d_requiredTokens__;
+        size_t      d_acceptedTokens__;
+        int         d_token__;
+        int         d_nextToken__;
+        size_t      d_state__;
+        STYPE__    *d_vsp__;
+        STYPE__     d_val__;
 
         ParserBase();
 
-        void ABORT() const throw(Return);
-        void ACCEPT() const throw(Return);
-        void ERROR() const throw(ErrorRecovery);
+        void ABORT() const;
+        void ACCEPT() const;
+        void ERROR() const;
+        void checkEOF__() const;
         void clearin();
         bool debug() const;
-        void pop(size_t count = 1);
-        void push(size_t nextState);
-        void reduce(PI const &productionInfo);
-        size_t top() const;
+        void pop__(size_t count = 1);
+        void push__(size_t nextState);
+        void popToken__();
+        void pushToken__(int token);
+        void reduce__(PI__ const &productionInfo);
+        void errorVerbose__();
+        size_t top__() const;
 
     public:
         void setDebug(bool mode);
@@ -69,13 +75,29 @@ class ParserBase
 
 inline bool ParserBase::debug() const
 {
-    return d_debug;
+    return d_debug__;
 }
 
 inline void ParserBase::setDebug(bool mode)
 {
-    d_debug = mode;
+    d_debug__ = mode;
 }
+
+inline void ParserBase::ABORT() const
+{
+    throw PARSE_ABORT__;
+}
+
+inline void ParserBase::ACCEPT() const
+{
+    throw PARSE_ACCEPT__;
+}
+
+inline void ParserBase::ERROR() const
+{
+    throw UNEXPECTED_TOKEN__;
+}
+
 
 // As a convenience, when including ParserBase.h its symbols are available as
 // symbols in the class Parser, too.
