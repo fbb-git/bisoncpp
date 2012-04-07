d7d13d11063f946141deafc18cde731c329a61d3
Frank B. Brokken <f.b.brokken@rug.nl>
2012-04-07 23:40:24 +0200

    STYPE__ rules not returning tagged values generate warnings

--- bisonc++/parser/checkfirsttype.cc
+++ bisonc++/parser/checkfirsttype.cc
@@ -8,15 +8,29 @@ void Parser::checkFirstType()
 
     // the production has elements, but no action. 
     // Now check if the production's FIRST element's stype is equal
-    // to the rule's stype.
+    // to the rule's stype or whether the rule's stype is empty (default)
 
+    
     string const *firstStype = &d_rules.sType(1);
 
-    if (firstStype->empty())
-        firstStype = &STYPE;
+//    if (firstStype->empty())
+//        firstStype = &STYPE;
 
-    if (stype.length() && stype != *firstStype)
+    if (d_semType == POLYMORPHIC)
+    {
+        if (stype == STYPE && firstStype->empty())
+            wmsg << 
+                "rule `" << &d_rules.lastProduction() << ":\n"
+                "\t\texplicitly tagged or `STYPE__' semantic value "
+                                                    "expected" << endl;
+    }
+    else if (stype.length() && stype != *firstStype)
         wmsg << 
             "rule `" << d_rules.name() << "' type clash (`" << 
             stype << "' `" << *firstStype << "') on default action" << endl;
 }
+
+
+
+
+
