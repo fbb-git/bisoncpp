94d6fe93f83a43708dca42ce263c779c1f080870
Frank B. Brokken <f.b.brokken@rug.nl>
2012-04-13 16:00:01 +0200

    Suppressed warning at auto tag == explicit tag overrides

--- bisonc++/parser/warnautooverride.cc
+++ bisonc++/parser/warnautooverride.cc
@@ -2,6 +2,9 @@
 
 void Parser::warnAutoOverride(AtDollar const &atd) const
 {
+    if (atd.text() != d_rules.sType())
+        return;
+
     wmsg.setLineNr(atd.lineNr());
     wmsg << &d_rules.lastProduction() << ":\n"
             "\t\t`" << atd.text() << "' overrides auto tag <" << 
