cd6bcf8bf33886b80f630ea73b082198c0d7de42
Frank B. Brokken <f.b.brokken@rug.nl>
2015-05-13 19:42:17 +0200

    REBIS OK: streamlined State::computeLAsets

--- bisonc++/state/computelasets.cc
+++ bisonc++/state/computelasets.cc
@@ -1,11 +1,17 @@
 #include "state.ih"
 
+// compute the LA sets of the items of a state.
+
+// starting from the state's kernel items distribute their LA sets over the
+// remaining items. See documentation/manual/algorithm/determine.yo
+
 void State::computeLAsets() 
 {
-    for_each(d_itemVector.begin(), d_itemVector.begin() + d_nKernelItems,
-        [&](StateItem &kernelItem)
-        {
-            distributeLAsetOf(kernelItem);
-        }
-    );
+    for 
+    (
+        auto kernelItem = &d_itemVector[0], end = kernelItem + d_nKernelItems; 
+            kernelItem != end;
+                ++kernelItem
+    )
+        distributeLAsetOf(*kernelItem);
 }
