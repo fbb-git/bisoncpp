abb3f1b48040eb71e526b166f8ddec91e876ae1c
Frank B. Brokken <f.b.brokken@rug.nl>
2016-03-23 11:21:42 +0100

    added missing parser/handlestype.cc

---
+++ bisonc++/parser/handlestype.cc
@@ -0,0 +1,35 @@
+#include "parser.ih"
+
+// See 'README.dollar' in this directory for a description of the actions
+// involved at these alternatives.
+
+//FBB: see returnPolymorphic and substitutedollar
+
+void Parser::handleSTYPE(Block &block, AtDollar const &atd) 
+{
+    string const &stype = d_rules.sType();
+
+    if (d_semType != POLYMORPHIC)
+    {
+        emsg << '`' << &d_rules.lastProduction() << 
+                "':  $$(...) requires %polymorphic" << endl;
+        return;
+    }
+
+    if (stype.empty())
+    {
+        emsg << '`' << &d_rules.lastProduction() << 
+                "':  $$(...) requires %type-tagged production rules" << endl;
+    }
+
+    string replacement = s_semanticValue;
+    replacement += 
+                    " = STYPE__::semantic<Tag__::" + d_rules.sType() + ">(";
+
+                                        // replace $$ by the semantic value
+    block.replace(atd.pos(), atd.length(), replacement);
+}
+
+
+
+
