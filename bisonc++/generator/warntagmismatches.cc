1c468a0129c9d6d9b2564bba002a3a5ec3e0a89f
Frank B. Brokken <f.b.brokken@rug.nl>
2016-03-24 17:11:31 +0100

    WIP updating manual and man-page

---
+++ bisonc++/generator/warntagmismatches.cc
@@ -0,0 +1,22 @@
+#include "generator.ih"
+
+void Generator::warnTagMismatches(ostream &out) const
+{
+    if (d_options.tagMismatches().value != Options::ON)
+        return;
+
+    key(out);
+
+    // idOfTag: defined by staticdata.cc
+
+    out << R"(
+    if (tag() != tg)
+    {
+        std::cerr << "[Fatal] calling `.get<Tag__::" << 
+            idOfTag__[static_cast<int>(tg)] << 
+            ">()', but Tag " <<
+            idOfTag__[static_cast<int>(tag())] << " is available\n";
+        throw 1;        // ABORTs
+    }
+    )" << '\n';
+}
