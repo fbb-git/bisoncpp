430b5874c8ac465e5ee214852e598ee1e5613129
Frank B. Brokken <f.b.brokken@rug.nl>
2016-04-06 12:15:48 +0530

    adapted examples/rpn to C++14

---
+++ bisonc++/documentation/manual/examples/rpn/main.cc
@@ -0,0 +1,8 @@
+#include "rpn.ih"
+
+int main()
+{
+    Parser parser;
+
+    parser.parse();
+}
--- bisonc++/documentation/manual/examples/rpn/parser/lex.cc
+++ bisonc++/documentation/manual/examples/rpn/parser/lex.cc
@@ -11,18 +11,19 @@ int Parser::lex()
 {
     char c;
                                     // get the next non-ws character
-    while (std::cin.get(c) && c == ' ' || c == '\t')            
+    while (std::cin.get(c) && (c == ' ' || c == '\t'))
         ;
 
-    if (!std::cin)                   // no characters were obtained
+    if (!std::cin)                  // no characters were obtained
         return 0;                   // indicate End Of Input  
     
-    if (c == '.' || isdigit (c))    // if a digit char was found
+    if (c == '.' || isdigit(c))     // if a digit char was found
     {
         std::cin.putback(c);        // return the character
-        std::cin >> d_val;          // extract a number
+        std::cin >> d_val__;        // extract a number
         return NUM;                 // return the NUM token
     }
 
     return c;                       // otherwise return the extracted char.
 }
+
