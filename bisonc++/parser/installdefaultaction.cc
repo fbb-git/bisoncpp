a93beb69b3d0fafb7b79d1ecb336eff7c01f33f6
Frank B. Brokken <f.b.brokken@rug.nl>
2016-04-25 10:37:12 +0530

    Fixed errors introduced by yodl 3.07.01, fixed linenr of default actions

--- bisonc++/parser/installdefaultaction.cc
+++ bisonc++/parser/installdefaultaction.cc
@@ -4,7 +4,7 @@ void Parser::installDefaultAction(Production const &prod,
                                   string const &rhs)
 {
     Block block;
-    block.open(prod.lineNr(), prod.fileName());
+    block.open(prod.lineNr() - 1, prod.fileName());
 
     block += "\n"
         "    " + s_semanticValue + " = " + rhs + ";\n"
