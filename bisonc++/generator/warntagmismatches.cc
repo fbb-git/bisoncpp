1bcdd1ac48c499632b9fe12f540ed884f014e918
Frank B. Brokken <f.b.brokken@rug.nl>
2017-05-12 13:22:27 +0200

    Rebis OK.     Polymorphic code now accesses d_nErrors via the global pointer     Meta__::t_nErrors, which is defined as a thread_local global variable     if the option --tread-safe was provided.
        With Polymorphic semantic values tag mismatches are silently
        accepted when errors were previously encountered.

--- bisonc++/generator/warntagmismatches.cc
+++ bisonc++/generator/warntagmismatches.cc
@@ -1,5 +1,26 @@
 #include "generator.ih"
 
+namespace {
+    char const *iftag =  
+R"(
+    if (tag() != tg)
+    {
+        if (*t_nErrors != 0)
+            const_cast<SType *>(this)->assign<tg>();
+        else
+        {
+            std::cerr << "[Fatal] calling `.get<Tag__::" << 
+                idOfTag__[static_cast<int>(tg)] << 
+                ">()', but Tag " <<
+                idOfTag__[static_cast<int>(tag())] << " is encountered. Try "
+                "option --debug and call setDebug(Parser::ACTIONCASES)\n";
+            throw 1;        // ABORTs
+        }
+    }
+)";
+
+} // namespace
+
 void Generator::warnTagMismatches(ostream &out) const
 {
     if (d_options.tagMismatches().value != Options::ON)
@@ -9,15 +30,6 @@ void Generator::warnTagMismatches(ostream &out) const
 
     // idOfTag: defined by staticdata.cc
 
-    out << R"(
-    if (tag() != tg)
-    {
-        std::cerr << "[Fatal] calling `.get<Tag__::" << 
-            idOfTag__[static_cast<int>(tg)] << 
-            ">()', but Tag " <<
-            idOfTag__[static_cast<int>(tag())] << " is encountered. "
-            " Try option --debug and call setDebug(Parser::ACTIONCASES)\n";
-        throw 1;        // ABORTs
-    }
-    )" << '\n';
+    out << iftag << '\n';
 }
+
